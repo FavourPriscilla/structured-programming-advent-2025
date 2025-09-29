088a70e32b30af28f8a8442cd5f10603ed9f4372
FavourPriscilla <mufavourpriscilla@gmail.com>
2025-09-29 16:19:18 +0300

    Add files via upload

---
+++ assignmengt on if-elif-else.py
@@ -0,0 +1,7 @@
+temp = float(input("Enter temperature in Fahrenheit: "))
+if temp > 80:
+     print("It's a hot day.")
+elif 60 <= temp <= 80:
+     print("It's a pleasant day.")
+else:
+     print("It's a cold day.")     
