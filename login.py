fb44fdf7536aab51f93b4852f11eb44aea986e91
FavourPriscilla <mufavourpriscilla@gmail.com>
2025-10-13 20:08:49 +0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+# Setting the right password
+right_password = "Favour2004"
+#Asking the user until the right password is entered
+user_input = ""
+while user_input != right_password:
+        user_input = input("Enter the password: ")
+print("Access granted") 
+
