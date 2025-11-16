5e8e17d66fe768898c53ca37eaafbb2482e5c448
FavourPriscilla <mufavourpriscilla@gmail.com>
2025-11-16 20:50:42 +0300

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,9 @@
+# This program asks the user for a number
+# and tells them whether it is even or odd.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
